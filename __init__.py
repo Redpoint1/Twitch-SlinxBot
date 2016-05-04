bc4689bee4464df5fe65675a07a4dd4443f70094
Redpoint1 <Redpoints1@gmail.com>
2016-05-04 23:38:01 +0200

    Version bumped to 0.1

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 __title__ = 'Twitcher'
-__version__ = '0.0.1'
+__version__ = '0.1.0'
 __author__ = 'Richard Rozar'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2016 Richard Rozar'
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='twitch-slinxbot',
-    version='0.0.1',
+    version='0.1.0',
     packages=[''],
     url='https://github.com/Redpoint1/Twitch-SlinxBot',
     license='MIT',
